bfcb8a6f7df6aa3f9cb6a26efc18176177493d18
minichell <dorna-michell.dadelahi@elev.ga.ntig.se>
2019-09-30 11:19:10 +0200

    R2

---
+++ V39/R2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class R2 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner (System.in);
+		int r1 = input.nextInt();
+		int s = input.nextInt();
+		System.out.println(2*s-r1);
+		
+		//test
+	}
+}
