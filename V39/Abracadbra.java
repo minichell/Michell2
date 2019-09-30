3207b7ddf80a882d7cae72cc82bee00066bb0e8e
minichell <dorna-michell.dadelahi@elev.ga.ntig.se>
2019-09-30 12:15:36 +0200

    Abracadbra

---
+++ V39/Abracadbra.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Abracadbra {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner Scanner = new Scanner(System.in);
+		int antal = Scanner.nextInt();
+		
+		
+		for(int iteration = 1;iteration <=antal; iteration++) {
+			System.out.println(iteration + " Abracadabra");
+	}
+	}
+}
